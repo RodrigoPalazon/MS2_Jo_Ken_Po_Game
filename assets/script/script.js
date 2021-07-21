bafd3ab6dbd112139a3a1f7e90617f98cb35c11e
Rodrigo Palazon <palazonbatera@gmail.com>
2021-07-21 10:51:04 +0200

    build: create battle logic comparing player and pc options

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,51 @@
 // alert('Halo Alle maal!');
 
 $(document).ready(function() {
-    $('#message').click(function(){
-        alert('Button working by jQuery');
+    let powers = ['rock', 'paper', 'scissors'];
+    let random = Math.floor(Math.random()*3);
+    let pc = powers[random];
+    console.log(pc);
+    
+    let player;
+    $('#rock').click(function(){
+        player = 'rock';
+        compare();
+        // console.log(player);
     });
+
+    $('#paper').click(function(){
+        player = 'paper';
+        compare();
+        // console.log(player);
+    });
+    $('#scissors').click(function(){
+        player = 'scissors';
+        compare();
+        // console.log(player);
+    });
+
+    function compare(){
+        if(pc === player){
+            console.log('TIED BATTLE');
+        }else if(pc === 'rock'){
+                if(player === 'paper'){
+                    console.log('Human WON');
+                }else{
+                    console.log('Robot WON');
+                }
+        }else if(pc === 'paper'){
+                if(player === 'scissors'){
+                    console.log('Human WON');
+                }else{
+                    console.log('Robot WON');
+                }
+        }else if(pc === 'scissors'){
+            if(player === 'rock'){
+            console.log('Human WON');
+            }else{
+            console.log('Robot WON');
+            }
+        }
+    }
 });
 
