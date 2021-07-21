79343272e8d9f2599f106771e7c3fd6c0857b6d7
Rodrigo Palazon <palazonbatera@gmail.com>
2021-07-21 17:01:14 +0200

    feat: show message 'JOKENPO' before show the winner

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,49 +4,66 @@ $(document).ready(function() {
     let pc = powers[random];
     console.log(pc);
     
-    let player;
-    $('#rock').click(function(){
-        player = 'rock';
-        compare();
-        // console.log(player);
-    });
+    
+        let player;
+        let options = ['#rock', '#paper', '#scissors'];
+        
+        $('#rock').click(function(){
+            player = 'rock';
+            displayJokenpoMessage();
+            compare();
+        });
 
-    $('#paper').click(function(){
-        player = 'paper';
-        compare();
-        // console.log(player);
-    });
-    $('#scissors').click(function(){
-        player = 'scissors';
-        compare();
-        // console.log(player);
-    });
+        $('#paper').click(function(){
+            player = 'paper';
+            displayJokenpoMessage();
+            compare();
+        });
+        $('#scissors').click(function(){
+            player = 'scissors';
+            displayJokenpoMessage();
+            compare();
+        });
+    
 
     function compare(){
         let messageDiv = $("#battleResult");
-        let message = ["<b>TIED BATTLE</b>", "<b>HUMAN WON</b>", "<b>ROBOT WON</b>"]
+        let message = ["<b>TIED BATTLE</b>", "<b>HUMAN WON</b>", "<b>ROBOT WON</b>"];
 
-        if(pc === player){
-            messageDiv.html(message[0]);
-        }else if(pc === 'rock'){
-                if(player === 'paper'){
+        setTimeout(function(){
+            if(pc === player){
+                messageDiv.html(message[0]);
+            }else if(pc === 'rock'){
+                    if(player === 'paper'){
+                        messageDiv.html(message[1]);
+                    }else{
+                        messageDiv.html(message[2]);
+                    }
+            }else if(pc === 'paper'){
+                    if(player === 'scissors'){
+                        messageDiv.html(message[1]);
+                    }else{
+                        messageDiv.html(message[2]);
+                    }
+            }else if(pc === 'scissors'){
+                if(player === 'rock'){
                     messageDiv.html(message[1]);
                 }else{
                     messageDiv.html(message[2]);
                 }
-        }else if(pc === 'paper'){
-                if(player === 'scissors'){
-                    messageDiv.html(message[1]);
-                }else{
-                    messageDiv.html(message[2]);
-                }
-        }else if(pc === 'scissors'){
-            if(player === 'rock'){
-                messageDiv.html(message[1]);
-            }else{
-                messageDiv.html(message[2]);
             }
-        }
+
+        }, 1500);
     }
+
+    let displayJokenpoMessage = () => {
+        let syllables = ['#item1', '#item2', '#item3'];
+        // for (syllable of syllables) {
+            setTimeout(function(){ 
+                $(".jokenpoMessage").css({"display": "inline-flex"}); 
+            }, 1000);
+        // }
+    }
+
 });
 
