96cac9f0dcbf1b2960928cae2276c0d67b9f89ed
Rodrigo Palazon <palazonbatera@gmail.com>
2021-07-22 17:25:04 +0200

    feat: increase player or pc points

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,69 +1,104 @@
 $(document).ready(function() {
+//...... PC random choice; 
     let powers = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random()*3);
     let pc = powers[random];
     console.log(pc);
     
-    
-        let player;
-        let options = ['#rock', '#paper', '#scissors'];
+    let player;
+    let options = ['#rock', '#paper', '#scissors'];
+    let result;
         
         $('#rock').click(function(){
             player = 'rock';
-            displayJokenpoMessage();
-            compare();
+            play();            
         });
 
         $('#paper').click(function(){
             player = 'paper';
-            displayJokenpoMessage();
-            compare();
+            play();
         });
+        
         $('#scissors').click(function(){
             player = 'scissors';
-            displayJokenpoMessage();
-            compare();
+            play();
         });
-    
 
-    function compare(){
+        function play(){
+            displayJokenpoMessage();
+            setTimeout(()=>{
+                increasePoint(compare(pc,player));
+            }, 500);
+        }
+
+  
+    function compare(pc,player){
         let messageDiv = $("#battleResult");
-        let message = ["<b>TIED BATTLE</b>", "<b>HUMAN WON</b>", "<b>ROBOT WON</b>"];
-
-        setTimeout(function(){
-            if(pc === player){
-                messageDiv.html(message[0]);
-            }else if(pc === 'rock'){
-                    if(player === 'paper'){
-                        messageDiv.html(message[1]);
+        let message = ["TIED BATTLE", "HUMAN WON", "ROBOT WON"];
+       
+        // let player;
+        // let pc;
+        
+        //    setTimeout(()=>{
+                if(pc === player){
+                    messageDiv.html(message[0]);
+                    
+                }else if(pc === 'rock'){
+                        if(player === 'paper'){
+                            messageDiv.text(message[1]);
+                            // humanPoints += 1;
+                            return message[1];
+                        }else{
+                            messageDiv.text(message[2]);
+                            return message[2];
+                        }
+                }else if(pc === 'paper'){
+                        if(player === 'scissors'){
+                            messageDiv.text(message[1]);
+                            return message[1];
+                        }else{
+                            messageDiv.text(message[2]);
+                            return message[2];
+                        }
+                }else if(pc === 'scissors'){
+                    if(player === 'rock'){
+                        messageDiv.text(message[1]);
+                        return message[1];
                     }else{
-                        messageDiv.html(message[2]);
+                        messageDiv.text(message[2]);
+                        return message[2];
                     }
-            }else if(pc === 'paper'){
-                    if(player === 'scissors'){
-                        messageDiv.html(message[1]);
-                    }else{
-                        messageDiv.html(message[2]);
-                    }
-            }else if(pc === 'scissors'){
-                if(player === 'rock'){
-                    messageDiv.html(message[1]);
-                }else{
-                    messageDiv.html(message[2]);
                 }
-            }
-
-        }, 1500);
-    }
+            // increasePoint();
+            // }, 1500);
+        }
+        
 
     let displayJokenpoMessage = () => {
-        let syllables = ['#item1', '#item2', '#item3'];
+        // let syllables = ['#item1', '#item2', '#item3'];
         // for (syllable of syllables) {
-            setTimeout(function(){ 
+            // setTimeout(function(){ 
                 $(".jokenpoMessage").css({"display": "inline-flex"}); 
-            }, 1000);
+            // }, 500);
         // }
     }
-
+    
+    function increasePoint (point){
+        // setTimeout(function(){
+            let message = ["TIED BATTLE", "HUMAN WON", "ROBOT WON"];
+            let humanPoints2 = $("#humanPoints").text();
+            let robotPoints2 = $("#robotPoints").text();
+            
+            if(point === message[1]){
+                humanPoints2++;
+                let print = $("#humanPoints").text(humanPoints2);
+            }else if(point === message[2]){
+                robotPoints2++;
+                let print = $("#robotPoints").text(robotPoints2);
+            }else{
+                console.log("no points");
+            }
+        // }, 3500);
+    }
 });
 
