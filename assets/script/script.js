7e940f997302859895eb05fb657e5bf78dffbc0c
Rodrigo Palazon <palazonbatera@gmail.com>
2021-08-05 15:06:03 +0200

    refactor: Create a new function switchLevel to manage the button's levels style

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,235 +1,228 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Switch the button's text in  menu.html
+  let difficultyBtn = "#menuPageDifficulty";
+  let levels = ["EASY", "MEDIUM", "HARD"];
 
-// menu.html
-let difficultyBtn = '#menuPageDifficulty';
-let levels = ['EASY', 'MEDIUM', 'HARD'];
-
-// Switch the button's text  
-let i = 0;
-let j= i;
-j++;
-let buttonLevel = $(difficultyBtn).click(function(){
-    if(j == levels.length){
-        j=i;
-        $(difficultyBtn).text(levels[j]);
-        j++;
-        // console.log(levels[j]);
-        return levels[j];
-    }else{
-        // console.log(j);
-        $(difficultyBtn).text(levels[j]);
-        j++;
-        return levels[j];
-    }
-});
-
-
+  console.log(localStorage.getItem("levels"));
   
-//...... PC random choice; 
-        let powers = ['rock', 'paper', 'scissors'];
-        // generate a random index 
-        let pc = powers[Math.floor(Math.random()*3)];
-        console.log(pc);
+  let j = 0;
+  
+  let buttonLevel = $(difficultyBtn).click(function () {
+    j++;
+    $(difficultyBtn).text(levels[j]);
+    if (j === levels.length) {
+      j=0;
+    } 
+   });
 
-    let player;
-    let options = ['#rock', '#paper', '#scissors'];
-    let result;
-        
-    
-    let imagesPath = [ 'assets/images/rocks/rock_3.jpg', 
-                       'assets/images/papers/paper_4.jpg', 
-                       'assets/images/scissors/scissor_5.png'
-                    ];
+  //...... PC random choice;
+  let powers = ["rock", "paper", "scissors"];
+  // generate a random index
+  let pc = powers[Math.floor(Math.random() * 3)];
+  console.log(pc);
 
-    //Rock,Paper, Scissors
-    $('.power').click(function getId(btn_id){
+  let player;
+  let options = ["#rock", "#paper", "#scissors"];
+  let result;
 
-        // setTimeout(function(){ console.log('waiting to display the image'); }, 3000);
+  let imagesPath = [
+    "assets/images/rocks/rock_3.jpg",
+    "assets/images/papers/paper_4.jpg",
+    "assets/images/scissors/scissor_5.png",
+  ];
 
-        btn_id = this.id;
-        player = btn_id;
-        console.log("The ID chosen is "+ player);
+  //.....Recognize by its id,  which button was clicked (Rock,Paper, Scissors).
+  $(".power").click(function getId(btn_id) {
+    // setTimeout(function(){ console.log('waiting to display the image'); }, 3000);
 
-        let humanImg = $('#humanPowerImage');
-        let robotImg = $('#robotPowerImage');
-        
-        $(humanImg).css("display", "block");
-        $(robotImg).css("display", "block");
+    btn_id = this.id;
+    player = btn_id;
+    console.log("The ID chosen is " + player);
 
-        if(btn_id === 'rock'){
-            humanImg.attr('src', imagesPath[0]);
-        }else if(btn_id === 'paper'){
-            humanImg.attr('src', imagesPath[1]);
-        }else{
-            humanImg.attr('src', imagesPath[2]);
-        }
+    let humanImg = $("#humanPowerImage");
+    let robotImg = $("#robotPowerImage");
 
-        if(pc === 'rock'){
-            robotImg.attr('src', imagesPath[0]);
-        }else if(pc === 'paper'){
-            robotImg.attr('src', imagesPath[1]);
-        }else{
-            robotImg.attr('src', imagesPath[2]);
-        }
-        play();
+    $(humanImg).css("display", "block");
+    $(robotImg).css("display", "block");
+    $("#versusImg").css("display", "block");
+    setTimeout(function () {
+      $("#versusImg").css("display", "none");
+    }, 2500);
 
-        setTimeout(function(){
-            $(humanImg).css("display", "none");
-            $(robotImg).css("display", "none");
-        }, 2500);
-        
-        // return btn_id;
-    });
-
-    if(buttonLevel === 'EASY'){
-        $('#easyLevel').click();
-    }
-    // $('#rock').click(function(){
-        //     player = 'rock';
-        //     play();  
-        //     // player = '';       
-        // });
-
-        // $('#paper').click(function(){
-        //     player = 'paper';
-        //     play();
-        // });
-        
-        // $('#scissors').click(function(){
-        //     player = 'scissors';
-        //     play();
-        // });
-
-        function play(){
-            displayJokenpoMessage();
-            setTimeout(()=>{
-                increasePoint(compare(pc,player));
-                player;
-                pc = powers[Math.floor(Math.random()*3)];
-                console.log(pc);
-            }, 500);
-        }
-
-
-    function compare(pc,player){
-        let messageDiv = $("#battleResultHumanPoint");
-        let messageDivRobot = $("#battleResultRobotPoint");
-        let message = ["TIED BATTLE", "HUMAN WON", "ROBOT WON"];
-        let plusOneStr = '+1 POINT';
-       
-            function timerMessage(){
-                setTimeout(() => {
-                    messageDiv.text('');
-                    messageDivRobot.text('');
-                }, 1500);
-            }
-        // let player;
-        // let pc;
-        
-        //    setTimeout(()=>{
-                if(pc === player){
-                    messageDiv.html(message[0]);
-                    messageDivRobot.html(message[0]);
-                    timerMessage();
-                    return message[0];
-                }else if(pc === 'rock'){
-                        if(player === 'paper'){
-                            messageDiv.text(plusOneStr);
-                            timerMessage();
-                            return message[1];
-                        }else{
-                            messageDivRobot.text(plusOneStr);
-                            timerMessage();
-                            return message[2];
-                        }
-                }else if(pc === 'paper'){
-                        if(player === 'scissors'){
-                            messageDiv.text(plusOneStr);
-                            timerMessage();
-                            return message[1];
-                        }else{
-                            messageDivRobot.text(plusOneStr);
-                            timerMessage();
-                            return message[2];
-                        }
-                }else if(pc === 'scissors'){
-                    if(player === 'rock'){
-                        messageDiv.text(plusOneStr);
-                        timerMessage();
-                        return message[1];
-                    }else{
-                        messageDivRobot.text(plusOneStr);
-                        timerMessage();
-                        return message[2];
-                    }
-                }
-            // increasePoint();
-            // }, 1500);
-           
-        }
-        
-
-    let displayJokenpoMessage = () => {
-      $("#jokenpo").css({"display": "block"}); 
-      setTimeout(function(){
-            $("#jokenpo").css({"display": "none"}); 
-        },2500); 
-    }
-    
-    function increasePoint (point){
-        // setTimeout(function(){
-            let message = ["TIED BATTLE", "HUMAN WON", "ROBOT WON"];
-            let humanPoints2 = $("#humanPoints").text();
-            let robotPoints2 = $("#robotPoints").text();
-            
-            if(point === message[1]){
-                humanPoints2++;
-                let print = $("#humanPoints").text(humanPoints2);
-                
-                if(humanPoints2 === parseInt($('.difficultyPoints').text()[0])){
-                    console.log('END OF THE GAME, Human won!');
-                    $('#trasparentDiv').css({"display": "block"});
-                    $('#winnerMessage').text('HUMAN WON');
-                }
-            }else if(point === message[2]){
-                robotPoints2++;
-                let print = $("#robotPoints").text(robotPoints2);
-
-                if(robotPoints2 === parseInt($('.difficultyPoints').text()[0])){
-                    console.log('END OF THE GAME, Robot won!')
-                    $('#trasparentDiv').css({"display": "block"});
-                    $('#winnerMessage').text('ROBOT WON');
-                }
-            }else{
-                console.log("no points");
-            }
-        // }, 3500);
+    if (btn_id === "rock") {
+      humanImg.attr("src", imagesPath[0]);
+    } else if (btn_id === "paper") {
+      humanImg.attr("src", imagesPath[1]);
+    } else {
+      humanImg.attr("src", imagesPath[2]);
     }
 
-    // Difficulty buttons 
+    if (pc === "rock") {
+      robotImg.attr("src", imagesPath[0]);
+    } else if (pc === "paper") {
+      robotImg.attr("src", imagesPath[1]);
+    } else {
+      robotImg.attr("src", imagesPath[2]);
+    }
+    play();
 
-    $('#easyLevel').click(function(){
-        $('.difficultyPoints').text('3');
-        $('#mediumLevel').removeClass('btn-success');
-        $('#hardLevel').removeClass('btn-success');
-        $(this).addClass('btn-success');
-    });
-    $('#easyLevel').click();
+    setTimeout(function () {
+      $(humanImg).css("display", "none");
+      $(robotImg).css("display", "none");
+    }, 2500);
 
-    $('#mediumLevel').click(function(){
-        $('.difficultyPoints').text('5');
-        $('#easyLevel').removeClass('btn-success');
-        $('#hardLevel').removeClass('btn-success');
-        $(this).addClass('btn-success');
-    });
-    $('#hardLevel').click(function(){
-        $('.difficultyPoints').text('9');
-        $('#easyLevel').removeClass('btn-success');
-        $('#mediumLevel').removeClass('btn-success');
-        $(this).addClass('btn-success');
-    });
+    // return btn_id;
+  });
+
+  if (buttonLevel === "EASY") {
+    $("#easyLevel").click();
+  }
+
+  // Trigger for the functions:
+  // -displayJokenpoMessage();
+  // -increasePoint();
+  // -compare();
+
+  function play() {
+    displayJokenpoMessage();
+    setTimeout(() => {
+      increasePoint(compare(pc, player));
+      player;
+      pc = powers[Math.floor(Math.random() * 3)];
+      console.log(pc);
+    }, 500);
+  }
+
+  // Compare the players' results
+  function compare(pc, player) {
+    let messageDiv = $("#battleResultHumanPoint");
+    let messageDivRobot = $("#battleResultRobotPoint");
+    let message = ["TIED BATTLE", "HUMAN WON", "ROBOT WON"];
+    let plusOneStr = "+1 POINT";
+
+    function timerMessage() {
+      setTimeout(() => {
+        messageDiv.text("");
+        messageDivRobot.text("");
+      }, 2000);
+    }
+    // let player;
+    // let pc;
+
+    //    setTimeout(()=>{
+    if (pc === player) {
+      messageDiv.html(message[0]);
+      messageDivRobot.html(message[0]);
+      timerMessage();
+      return message[0];
+    } else if (pc === "rock") {
+      if (player === "paper") {
+        messageDiv.text(plusOneStr);
+        timerMessage();
+        return message[1];
+      } else {
+        messageDivRobot.text(plusOneStr);
+        timerMessage();
+        return message[2];
+      }
+    } else if (pc === "paper") {
+      if (player === "scissors") {
+        messageDiv.text(plusOneStr);
+        timerMessage();
+        return message[1];
+      } else {
+        messageDivRobot.text(plusOneStr);
+        timerMessage();
+        return message[2];
+      }
+    } else if (pc === "scissors") {
+      if (player === "rock") {
+        messageDiv.text(plusOneStr);
+        timerMessage();
+        return message[1];
+      } else {
+        messageDivRobot.text(plusOneStr);
+        timerMessage();
+        return message[2];
+      }
+    }
+    // increasePoint();
+    // }, 1500);
+  }
+
+  // Display a text when some power button is clicked.
+  let displayJokenpoMessage = () => {
+    $("#jokenpo").css({ display: "block" });
+    setTimeout(function () {
+      $("#jokenpo").css({ display: "none" });
+    }, 2500);
+  };
+
+  // Increase the points and display a div in the
+  // end of the game showing how is the winner
+  function increasePoint(point) {
+    // setTimeout(function(){
+    let message = ["TIED BATTLE", "HUMAN WON", "ROBOT WON"];
+    let humanPoints2 = $("#humanPoints").text();
+    let robotPoints2 = $("#robotPoints").text();
+
+    if (point === message[1]) {
+      humanPoints2++;
+      let print = $("#humanPoints").text(humanPoints2);
+
+      if (humanPoints2 === parseInt($(".difficultyPoints").text()[0])) {
+        console.log("END OF THE GAME, Human won!");
+        $("#trasparentDiv").css({ display: "block" });
+        $("#winnerMessage").text("HUMAN WON");
+      }
+    } else if (point === message[2]) {
+      robotPoints2++;
+      let print = $("#robotPoints").text(robotPoints2);
+
+      if (robotPoints2 === parseInt($(".difficultyPoints").text()[0])) {
+        console.log("END OF THE GAME, Robot won!");
+        $("#trasparentDiv").css({ display: "block" });
+        $("#winnerMessage").text("ROBOT WON");
+      }
+    } else {
+      console.log("no points");
+    }
+    // }, 3500);
+  }
+
+  // .....Difficulty buttons ......
+  // create a function outside and add the beneath code
+
+  function swicthLevel(element, point) {
+    $(".difficultyPoints").text(point.toString());
+    $(element).addClass("btn-danger");
     
-    
+    if(element.id === "easyLevel"){
+      $('#mediumLevel').removeClass("btn-danger");
+      $('#hardLevel').removeClass("btn-danger");
+    }else if(element.id === "mediumLevel"){
+      $('#easyLevel').removeClass("btn-danger");
+      $('#hardLevel').removeClass("btn-danger");
+    }else{
+      $('#easyLevel').removeClass("btn-danger");
+      $('#mediumLevel').removeClass("btn-danger");
+    }
+  }
 
+  $("#easyLevel").click(function () {
+    swicthLevel(this, 3);
+    localStorage.setItem("levels", "easy");
+  });
+
+  // $("#easyLevel").click();
+
+  $("#mediumLevel").click(function () {
+    swicthLevel(this, 5);
+    localStorage.setItem("levels", "medium");
+  });
+
+  $("#hardLevel").click(function () {
+    swicthLevel(this, 9);
+    localStorage.setItem("levels", "hard");
+  });
 });
-
